f6acbc3c4af27c7fc690a808f6c3284b170174bd
Rodrigo Silveira <r3850365@gmail.com>
2025-08-18 06:17:23 -0700

    Checking in our first library, this will evolve!

---
+++ Library/Code.js
@@ -0,0 +1,88 @@
+/**
+ * Removes duplicate rows from the current sheet.
+ */
+function removeDuplicates() {
+  const sheet = SpreadsheetApp.getActiveSheet();
+  const data = sheet.getDataRange().getValues();
+  const uniqueData = {};
+  for (let row of data) {
+    const key = row.join();
+    uniqueData[key] = uniqueData[key] || row;
+  }
+  sheet.clearContents();
+  const newData = Object.values(uniqueData);
+  sheet.getRange(1, 1, newData.length, newData[0].length).setValues(newData);
+}
+
+/* ********************************************************************************************************************* */
+// activateSheet
+//    Activates a sheet in the current spreadsheet
+// 
+// Input:
+//    sheetName (String) - The sheet name to activate
+// 
+// Output:
+//    sheet (Sheet) - The activated sheet, or null if not found
+//
+//* ********************************************************************************************************************* */
+// 
+function activateSheet(sheetName) {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = spreadsheet.getSheetByName(sheetName);
+  if (sheet) {
+    spreadsheet.setActiveSheet(sheet);
+  } else {
+    return null;
+  }
+  return sheet;
+}
+
+/* ********************************************************************************************************************* */
+// removeHeaderAndBlankRiows
+//    Remove header and blank rows from the named range. For each row, it examines row[0] ang filters it out if it is
+//    equal to "" or the name argument
+// 
+// Input:
+//    rangeName (String) - The range name where to apply the filter
+//    headerString - (String) - The undesired header string on the named column
+//    column (Int) - The column where to apply the filter
+//
+// Output:
+//    range (Range) - The filtered range, null if no range found
+//* ********************************************************************************************************************* */
+// 
+function removeHeaderAndBlankRows(rangeName, header, column) {
+  // const spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+  // const sheet = spreadSheet.getSheetByName(sheetName);
+  // const range = sheet.getRange("A1:D100");
+  // const filter = range.createFilter();
+  // Get the Range
+  const spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+  var range = spreadSheet.getRangeByName(rangeName);
+  if (!range) {
+    return null;
+  }
+
+  var filter;
+  // Create a filter in the Range
+  if (range.getFilter()) {
+    filter = range.getFilter();
+    filter.remove();
+  }
+  filter = range.createFilter();
+ 
+  // Set criteria, no hader string nor blanks
+  const criteria = SpreadsheetApp.newFilterCriteria().whenTextDoesNotContain(header).build();
+
+  // Apply the criteria to the filter
+  filter.setColumnFilterCriteria(column, criteria);
+
+  // Retrive the filtered data
+  range = filter.getRange();
+  var filteredData = range.getValues();
+
+  // Remove the filter 
+  // filter.remove();
+
+  return filteredData;
+}
---
+++ Library/ContasCorrentes.js
@@ -0,0 +1,157 @@
+// Contas Correntes - CC_SHEET_ID
+// 
+const CC_SHEET_ID								= "10QXCS1QspqKH8owJQiazFc1dSumWy94mgHIVhZargcA";
+const CC_TAB_NAME			    				= "ContasCorrentes"
+const contasCorentesRange						= "Dados";
+const contasCorrentesNome          				= "ContasCorrentesNome";
+const contasCorrentesEstadia        			= "ContasCorrentesEstadia";
+const CC_TRANSACOES_RENDAS_DESPESAS_RANGE_NAME	= "TransacoesRendasDepesas";  
+const contasCorrentesCreditoReal         	= "CreditoReal";
+const contasCorrentesCreditoOuro          	= "CreditoOuro";
+const contasCorrentesDebitoReal           	= "DebitoReal"; 
+const contasCorrentesDebitoOuro           	= "DebitoOuro";
+const contasCorrentesDataCol              	= 0;
+const contasCorrentesNomeCol             	= 1;
+const contasCorrentesEstadiaCol           	= 2;
+const contasCorrentesMetodoCol            	= 3;  // Diaria, Salario, Porcentagem, Cantina, PIX, Diversos
+const contasCorrentesMoedaCol             	= 4   // Real, Ouro
+const contasCorrentesCreditDebitCol       	= 5;  // Credito, Debito
+const contasCorrentesItemCol              	= 6;
+const contasCorrentesPrecoUnidadeRealCol  	= 7;  // Real
+const contasCorrentesPrecoUnidadeOuroCol  	= 8;  // Gramas de ouro 
+const contasCorrentesItemQtdCol          	 = 9;
+const contasCorrentesTotalRealCol         	= 10; // Real
+const contasCorrentesTotalOuroCol         	= 11; // Gramas de ouro
+const contasCorrentesComentariosCol       	= 12;
+
+function cc_getSpreadSheet() {
+  return SpreadsheetApp.openById(CC_SHEET_ID);
+}
+function cc_getContasCorrentesTab() {
+	let spreadSheet = getContasCorrentesSpreadSheet();
+	return spreadSheet.getSheetByName(CC_TAB_NAME);
+}
+function cc_getDadosSheet() {
+	let spreadSheet = getContasCorrentesSpreadSheet();
+	return spreadSheet.getSheetByName(DADOS_TAB_NAME);
+}
+function cc_getDadosSheetRange() {
+	let spreadSheet = getContasCorrentesSpreadSheet();
+	return spreadSheet.getRangeByName(CC_TRANSACOES_RENDAS_DESPESAS_RANGE_NAME);
+}
+function cc_getTransacoesRendasDespesasRange() {
+	let spreadSheet = getContasCorrentesSpreadSheet();
+	return spreadSheet.getRangeByName(CC_TRANSACOES_RENDAS_DESPESAS_RANGE_NAME);
+}
+
+/* ********************************************************************************************************************* */
+// resumirContaCorrentAssociado
+//    Calcula um resumo dos Creditos e Debitos de um Associado, in Real e Ouro
+// 
+// Input:
+//    nome (String)       - O nome to Associado
+//    estadia (Date       - (Date) - A data da estadia do Associado
+//    transacoes (Range)  - A fonte dos dados a serem sumarizados
+//
+// Output:
+//    creditosAndDebitosRealOuro (Object) - Um object com os sumarios desejados, ou null in caso de erro:
+//    var creditosAndDebitosRealOuro = {
+//      Credito: {
+//        Real: 0,
+//         Ouro: 0
+//      },
+//      Debito: {
+//        Real: 0,
+//        Ouro: 0
+//      },
+//    }
+//* ********************************************************************************************************************* */
+//
+function resumirContaCorrenteAssociado (nome, estadia, transactions) {
+  // Iniializamos os sumarios 
+    var creditosDebitosRealOuro = {
+    Credito: {
+      Real: 0,
+      Ouro: 0
+    },
+    Debito: {
+      Real: 0,
+      Ouro: 0
+    },
+  }
+
+  // Campos de trabalho para compararmos a estadia desejada com a estadia dos 
+  var estadiaDia = new Date(estadia).getDay();
+  var estadiaMes = new Date(estadia).getMonth();
+  var estadiaAno = new Date(estadia).getFullYear();
+
+  if (transactions.length == 0) {
+    var message = "";
+    message += "Nao ha nehuma trasacao a ser processada";
+    return null;
+  }
+  var filteredTransactions = transactions.filter(function(transaction) {
+    return transaction[contasCorrentesNomeCol] == nome &&
+           transaction[contasCorrentesDataCol] != "" &&
+           new Date(transaction[contasCorrentesEstadiaCol]).getDay() == estadiaDia &&
+           new Date(transaction[contasCorrentesEstadiaCol]).getMonth() == estadiaMes &&
+           new Date(transaction[contasCorrentesEstadiaCol]).getFullYear() == estadiaAno;
+  });
+  if (filteredTransactions.length == 0) {
+    return creditosDebitosRealOuro;
+  }
+
+  for (var i=0; i < filteredTransactions.length; i++) {
+    var creditoDebito = filteredTransactions[i][contasCorrentesCreditDebitCol]
+    switch (creditoDebito) {
+      case "Credito":
+            var moeda = filteredTransactions[i][contasCorrentesMoedaCol];
+            switch (moeda) {
+          case "Real":
+            creditosDebitosRealOuro["Credito"]["Real"] += filteredTransactions[i][contasCorrentesTotalRealCol];
+            break;
+          case "Ouro":
+            creditosDebitosRealOuro["Credito"]["Ouro"] += filteredTransactions[i][contasCorrentesTotalOuroCol];
+            break;
+          default:
+            var message = "";
+            message += "Valor do atributo Moeda invalido (";
+            message += filteredTransactions[i][moedaCol];
+            message += ") na linha #"
+            message += i;
+            message += " na matrix de transacoes";
+            return null;
+        }
+        break;
+      case "Debito":
+            var moeda = filteredTransactions[i][contasCorrentesMoedaCol];
+        switch (moeda) {
+          case "Real":
+            creditosDebitosRealOuro["Debito"]["Real"] += filteredTransactions[i][contasCorrentesTotalRealCol];
+            break;
+         case "Ouro":
+            creditosDebitosRealOuro["Debito"]["Ouro"] += filteredTransactions[i][contasCorrentesTotalOuroCol];
+            break;
+          default:
+            var message = "";
+            message += "Valor do atributo Moeda invalido (";
+            message += filteredTransactions[i][contasCorrentesMoedaCol];
+            message += ") na linha #"
+            message += i;
+            message += " na matrix de transacoes";
+            return null;
+        }
+        break;
+     default:
+        var message = "";
+        message += "Valor do atributo Credito/Debito invalido (";
+        message += filteredTransactions[i][contasCorrentesCreditDebitCol];
+        message += ") na linha #"
+        message += i;
+        message += " na matrix de transacoes"
+        console.error(message)
+        return null;
+    }
+  }
+  return creditosDebitosRealOuro;
+}
