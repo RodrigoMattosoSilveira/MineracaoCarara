e846a9310c9f98aeeb28daf6e8756d676b64fac6
Rodrigo Silveira <r3850365@gmail.com>
2025-09-07 09:58:11 -0700

    Tested obterProducaoPocos in Cronograma

--- Cronograma/Test.js
+++ Cronograma/Test.js
@@ -1,9 +1,39 @@
+const EPSILON = 0.00001;
+const obterProducaoPocosTestes = () => {
+  let dataProcurada = '9/5/2025'
+	let producao = Producao.obterProducaoPocos("9/5/2025");
+	let producaoKeys = Object.keys(producao);
+	producaoKeys.length === 2 ?
+		console.info(" ✔︎ obterProducaoPocos, " + dataProcurada + " length === 2") :
+		console.error(" ✖︎  obterProducaoPocos, " + dataProcurada + " length === 2")
+	Math.abs(producao["Poço_1"] - 21.9534838229411) < EPSILON ?
+		console.info(" ✔︎ obterProducaoPocos, Poço_1" + dataProcurada + " 21.9534838229411") :
+		console.error(" ✖︎  obterProducaoPocos, Poço_1" + dataProcurada + " 21.9534838229411")
+	Math.abs(producao["Poço_2"] - 0.710864316655835) < EPSILON ?
+		console.info(" ✔︎ obterProducaoPocos, Poço_2" + dataProcurada + " 7.98903561473896") :
+		console.error(" ✖︎  obterProducaoPocos, Poço_2 " + dataProcurada + " 7.98903561473896")	
+
+    dataProcurada = '1/1/2025'
+    producao = Producao.obterProducaoPocos("1/1/2025");
+	producaoKeys = Object.keys(producao);
+	producaoKeys.length === 1 ?
+		console.info(" ✔︎ obterProducaoPocos, " + dataProcurada + " length === 1") :
+		console.error(" ✖︎  obterProducaoPocos, " + dataProcurada + " length === 1");
+  producaoKeys.indexOf("Poço_1") !== -1 ?
+  	console.info(" ✔︎ obterProducaoPocosTeste, Poço_1 " + dataProcurada + " presente") :
+		console.error(" ✖︎  obterProducaoPocosTeste, Poço_1 " + dataProcurada + " resente");
+  producaoKeys.indexOf("Poço_2") === -1 ?
+  	console.info(" ✔︎ obterProducaoPocosTeste, Poço_2 " + dataProcurada + " nao presente") :
+		console.error(" ✖︎  obterProducaoPocosTeste, Poço_2 " + dataProcurada + " nao presente");
+	Math.abs(producao["Poço_1"] - 19.6178864851321) < EPSILON ?
+		console.info(" ✔︎ obterProducaoPocos, Poço_1 " + dataProcurada + "  21.9534838229411") :
+		console.error(" ✖︎  obterProducaoPocos, Poço_1 " + dataProcurada + " 21.9534838229411")	
+}
 const obterProducaoPocoTeste = () => {
-  let epsilon = 0.00001;
-  let pocoProducao = Production.obterProducaoPoco("Poço_1", "9/6/2025");
-  Math.abs(pocoProducao[PRODUCAO_QTD] - 15.97807122947792) < epsilon ?
+  let pocoProducao = Producao.obterProducaoPoco("Poço_1", "9/6/2025");
+  Math.abs(pocoProducao[PRODUCAO_QTD] - 15.97807122947792) < EPSILON ?
    	console.info(" ✔︎ obterProducaoPoco, Poço_1 9/6/2025  15.97807122947792") :
-		console.error(" ✖︎  obterProducaoPoco, Poço_1 9/6/2025  15.97807122947792")
+	console.error(" ✖︎  obterProducaoPoco, Poço_1 9/6/2025  15.97807122947792")
 }
 const obterProducaoPocosTeste = () => {
   let pocos = Producao.obterProducaoPocosNomes();
