74c206e6b1f97b5c5f37f605839bba1cab8f0535
Rodrigo Silveira <r3850365@gmail.com>
2025-09-07 09:06:32 -0700

    tests working here

--- Cronograma/Test.js
+++ Cronograma/Test.js
@@ -1,3 +1,24 @@
+const obterProducaoPocoTeste = () => {
+  let epsilon = 0.00001;
+  let pocoProducao = Production.obterProducaoPoco("Poço_1", "9/6/2025");
+  Math.abs(pocoProducao[PRODUCAO_QTD] - 15.97807122947792) < epsilon ?
+   	console.info(" ✔︎ obterProducaoPoco, Poço_1 9/6/2025  15.97807122947792") :
+		console.error(" ✖︎  obterProducaoPoco, Poço_1 9/6/2025  15.97807122947792")
+}
+const obterProducaoPocosTeste = () => {
+  let pocos = Producao.obterProducaoPocosNomes();
+  pocos.length === 2 ?
+  	console.info(" ✔︎ obterProducaoPocosTeste, length === 2") :
+		console.error(" ✖︎  obterProducaoPocosTeste, length === 2")
+  
+  pocos.indexOf("Poço_1") !== -1 ?
+  	console.info(" ✔︎ obterProducaoPocosTeste, Poço_1 presente") :
+		console.error(" ✖︎  obterProducaoPocosTeste, Poço_1 presente");
+
+  pocos.indexOf("Poço_3") === -1 ?
+  	console.info(" ✔︎ obterProducaoPocosTeste, Poço_3 not presente") :
+		console.error(" ✖︎  obterProducaoPocosTeste, Poço_3 not presente");
+}
 const sorAtivosMaisAntigoTeste = () => {
 	let ativosGama = obterAtivosGamaVals();
 	ativosGama = [...addicioneOrdermAoCronograma(ativosGama)];
