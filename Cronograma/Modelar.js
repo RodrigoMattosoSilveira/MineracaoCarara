9ba8b01e7fc53de79b06b2d6dc9513ba67e43f60
RodrigoMattosoSilveira <r3850365@gmail.com>
2025-09-14 09:06:29 -0700

    Spring Clearning

--- Cronograma/Modelar.js
+++ Cronograma/Modelar.js
@@ -17,7 +17,7 @@ function cronogramaModelar() {
 	let cronogramaPublicadoMaisRecente = [...obterCronogramaPublicadoMaisRecente()]
 
 	// Recuperar os períodos válidos. Usar "Diurno" caso não haja nenhum 
-	let periodosMap = Master.obterPeriodos();
+	let periodosMap = Referencia.obterPeriodos();
 
 	// Calcular o candidato ao proximo cronograma
 	let proximoCronogramaCandidato = [...obterProximoCronograma(cronogramaPublicadoMaisRecente, periodosMap)]
