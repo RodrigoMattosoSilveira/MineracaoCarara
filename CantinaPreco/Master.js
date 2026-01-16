53250c04f5d03594e333b682365b1a31c354392e
Rodrigo Silveira <r3850365@gmail.com>
2026-01-16 14:00:49 -0800

    Removed Referencia

---
+++ CantinaPreco/Master.js
@@ -0,0 +1,9 @@
+/* ********************************************************************************************************************* */
+//  Ler a cotacao do ouro, mantida em tempo real pela GOOGLEFINACE
+//  @returns {Number}, cotacaoOuro
+//
+//* ********************************************************************************************************************* */
+//
+function obtenhaCotacaoOuroSimples() {
+  return cotacaoOuro = Referencia.obterReferenciaOuroBrlGramaVal();
+}
