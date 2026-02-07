04099427c543023aa56e628e35572899257dd9fb
Rodrigo Silveira <r3850365@gmail.com>
2026-02-07 11:14:14 -0800

    The menu now has only 3 choices

--- Cronograma/Main.js
+++ Cronograma/Main.js
@@ -1,3 +1,41 @@
+/*
+Planejar: Cronograma!Anterior + Estadia!Dados ➞  Cronograma!Planejar:
+	Planejador seleciona o Menu Planejar
+	Sistema solicita ao planejador o Período a Planejar
+	Planejador seleciona o Período a Planejar
+	Sistema limpa a guia Planejar
+	Sistema copia Dados para Planejar ➞ No Anterior ? Acao = Verde : Acao = Vermelho
+	O planejador ajusta (Inclui/Exclui/Edita) registros na guia Planejar
+
+Informar: Cronograma!Planejar ➞ Cronograma!Ativos
+	Planejador seleciona o menu Informar
+	Sistema limpa a guia Ativos
+	Sistema preenche a guia Ativos com registros da guia Planejar
+	Sistema limpa a guia PDF
+	Sistema usa a guia Ativos para gerar a guia PDF
+	O Planejador seleciona a guia PDF
+	O Planejador imprime a guia PDF
+
+Contabilizar: Cronograma!Contabilizar ➞ ContasCorrentes!Dados
+	Ao concluir o trabalho e antes de contabilizar o trabalho realizado: 
+		O Planejador edita a guia Ativos, conforme necessário
+	O Planejador seleciona o menu Contabilizar
+	O sistema addiciona os registros da guia Ativos para a guia Anterior e para a guia Contabilizar
+	O sistema registra lançamentos no livro-razão:
+		Colaboradores Diária
+			com o valor da diária
+			marca o registro de Contabilizar para ser excluído
+		Colaboradores Salário
+			zero como total se não for o último dia do mês
+			salário se for o último dia do mês
+			marca o registro de Contabilizar para ser excluído
+		Colaboradores Porcentagem / M-Porcentagem
+			com percentual da produção do poço, com registro de produção do poço
+			marca o registro de Contabilizar para ser excluído
+	O sistema remove os registros marcados para exclusão da guia Contabilizar
+
+ */
+
 // ****************************************************************************
 // Menu - This is a set of functions
 // ****************************************************************************
@@ -7,10 +45,8 @@ function onOpen() {
 
   // Or DocumentApp, SlidesApp or FormApp.
 	ui.createMenu('Cronograma')
-		.addItem('Modelar', 'cronogramaModelar')
-		.addItem('Planejar', 'cronogramaPlanejar')
-		.addItem('Podar', 'cronogramaPodar')
-		.addItem('Informar', 'cronogramaInformar')
+		.addItem('Planejar',     'cronogramaPlanejar')
+		.addItem('Informar',     'cronogramaInformar')
 		.addItem('Contabilizar', 'cronogramaContabilizar')
 	.addToUi();
 }
