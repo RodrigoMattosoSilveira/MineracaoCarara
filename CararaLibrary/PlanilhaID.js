175fb35e8aefdaaf9ca0ec1b0ef370406d0072b0
Rodrigo Silveira <r3850365@gmail.com>
2026-03-12 08:23:16 -0300

    Not using hard coded spreadsheet id any longer

--- CararaLibrary/PlanilhaID.js
+++
@@ -1,5 +0,0 @@
-// This is the GOLD STANDARD for the IDs of the spreadsheets used in the Carara project.
-// Any change to these IDs should be done here, and all other code should refer to these constants.
-const ESTADIAS_ID         = "1cBWZwZ8JPJARGNFmjFAFzKIaPApeO5kN8jencYUVki4";
-const PRODUCAO_ID         = "1XJQmUb-1W2egXWj000Uer8QS6FZd7lUgqppoehjxwj4";
-const REFERENCIA_ID       = "1LJqg-R3ZNB-fC44QKKxjx8ubhHYzbPWEDCmPykHn-y8";
